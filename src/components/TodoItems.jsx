5e9150c0e89a762f99eddaaef80c957ee8927e02
ayyappagk <ayyappagk22@gmail.com>
2025-07-05 21:10:02 +0530

    modified todo component

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -8,7 +8,7 @@ const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
     <div className='flex items-center my-3 gap-2'>
         <div onClick={()=>{toggle(id)}} className='flex flex-1 items-center cursor-pointer'>
             <img src={isComplete ? tick: not_tick} alt='' className='w-7'/>
-            <p className={`text-slate-700 ml-4 text-[17px] decoration-slate-500
+            <p className={`text-slate-900 ml-4 text-[17px] decoration-slate-600
                  ${isComplete ? "line-through" : ""}`}>{text}
             </p>
         </div>
